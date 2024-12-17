e66145c2a641824780652d01d8d8e0b01c32a489
iman <imancx.cx@gmail.com>
2024-12-17 11:23:24 +0330

    ....

--- src/components/product-card.tsx
+++ src/components/product-card.tsx
@@ -26,21 +26,16 @@ type Props = {
 const ProductCard = ({ product }: Props) => {
     const addToCart = useCartStore(state => state.addToCart);
     const updateQty = useCartStore(state => state.updateQty);
-    const updateQty = useCartStore(state => state.updateQty);
     const cart = useCartStore(state => state.cart);
-    const totalPrice = useCartStore(state => state.totalPrice);
-    const totalItems = useCartStore(state => state.totalItems);
-    console.log({ cart, totalPrice, totalItems });
-
     const isInBasket = cart.some(item => item.id === product.id);
 
     return (
-        <div className="relative w-64 hover:shadow-md transition-all ease-in-out ">
+        <Card className="relative w-64 hover:shadow-md transition-all ease-in-out ">
             <div
                 onClick={() => (product.isFavorite ? removeFromFavorite(product.id) : addToFavorite(product.id))}
                 className="absolute cursor-pointer top-4 left-4"
             >
-                {product.isFavorite ? <FcLike /> : <FcLikePlaceholder />}
+                {product.isFavorite ? <FcLike /> : <PiHeartFill />}
             </div>
             <Image
                 width={200}
@@ -49,12 +44,10 @@ const ProductCard = ({ product }: Props) => {
                 alt={product.name}
                 className="w-full h-80 object-cover"
             />
-            <div className="flex flex-col p-2">
+            <CardContent className="flex flex-col p-2">
                 <h2 className="font-medium">{product.name}</h2>
                 <p className="text-[11px] font-light text-gray-400">{product.description} </p>
-                <div className="flex justify-between items-center mt-2">
-                    {/* check behse k product.id mojud has to array basket az zustand ?*/}
-
+                <CardFooter className="flex justify-between items-center mt-2">
                     {!isInBasket ? (
                         <button
                             className="text-[10px] bg-blue-500 p-2 rounded text-white"
@@ -86,14 +79,14 @@ const ProductCard = ({ product }: Props) => {
                                         onClick={() => updateQty("decrement", item.id)}
                                         className="bg-red-500 aspect-square px-2 rounded-lg  text-primary-content"
                                     >
-                                        {(item.quantity as number) > 1 ? "-" : <MdDelete />}
+                                        {(item.quantity as number) > 1 ? "-" : <RiDeleteBin5Line />}
                                     </button>
                                 </div>
                             ))
                     )}
 
                     <p className="">{product.price}</p>
-                </div>
+                </CardFooter>
             </CardContent>
         </Card>
     );
